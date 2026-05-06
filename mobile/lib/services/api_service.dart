58e5d35d1bd16baa5b5223f42ce31267c0df4b88
Jordy Bujaico <jordy.bujaicob@unmsm.edu.pe>
2026-05-06 11:05:51 -0500

    Mobile: Pantalla AddEstacionScreen

--- mobile/lib/services/api_service.dart
+++ mobile/lib/services/api_service.dart
@@ -1,20 +1,20 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../models/estacion.dart';
+import 'auth_service.dart';
 
 class ApiService {
-  // Nota: 10.0.2.2 es el localhost para el emulador Android.
-  // Si usa Linux Desktop o Web, use 'localhost'.
-  final String baseUrl = "http://127.0.0.1:8000";
+  final String baseUrl = "http://10.0.2.2:8000";
 
-  Future<List<Estacion>> fetchEstaciones() async {
-    final response = await http.get(Uri.parse('$baseUrl/estaciones/'));
-
-    if (response.statusCode == 200) {
-      List jsonResponse = json.decode(response.body);
-      return jsonResponse.map((data) => Estacion.fromJson(data)).toList();
-    } else {
-      throw Exception('Falla al conectar con el servidor SMAT');
-    }
+  Future crearEstacion(String nombre, String ubicacion) async {
+    final token = await AuthService().getToken();
+    final response = await http.post(
+      Uri.parse('$baseUrl/estaciones/'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode({'nombre': nombre, 'ubicacion': ubicacion}),
+    );
+    return response.statusCode == 200;
   }
 }
